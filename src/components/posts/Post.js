9642669c9d4171a3bfa4e97ca016957bbdeeacd9
Michael Dotson <thedotson@icloud.com>
2020-11-07 10:26:13 -0600

    adjusted view all posts to show first and last name as well as category name

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -8,9 +8,9 @@ class Post extends React.Component {
     const creation_date = moment(post.creation_date).format('MMM Do, YYY');
     return(
       <div className="post-list">
-        <h5>{post.first_name} {post.last_name}</h5>
+        <h5>{post.user.first_name} {post.user.last_name}</h5>
         <h5>{post.subject}</h5>
-        <h5>{post.category_id}</h5>
+        <h5>{post.category.name}</h5>
       </div>
     )
   }
