8cf19b1a2ec183587cb1ad20a055ee77da1cba24
Geno McNew <genemcnew3@gmail.com>
2020-11-09 21:08:09 -0600

    can delete comments and deletes comments when posts deleted

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import moment from 'moment';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
 
 class Comment extends React.Component {
+  
+
+  deletePostEvent = () => {
+    const { deleteComment, comment } = this.props
+    confirmAlert({
+      customUI: ({ onClose }) => {
+        return (
+          <div className="custom-ui">
+            <h1>Delete?</h1>
+            <p>Are you sure you want to proceed?</p>
+            <button className="mr-3 dialog-btn" onClick={onClose}><h4>No</h4></button>
+            <button className="dialog-btn"
+              onClick={() => {
+                deleteComment(comment.id);
+                onClose();}}>
+                  <h4>Yes, Delete this Comment</h4>
+            </button>
+          </div>
+        );
+      }
+    });
+  };
+
   render() {
     const { comment } = this.props;
     const creation_date = moment(comment.creation_date).format('MMM Do, yyyy');
-    console.warn(comment.user_id)
-    console.warn(localStorage.getItem("rare_user_id"))
     return(
       <div className="comment">
         <h5>{comment.subject}</h5>
-        <h6>{`${comment.user.first_name} ${comment.user.last_name} - ${creation_date}`}</h6>
+        <h6>{`${comment.user.first_name} ${comment.user.last_name} - ${creation_date}`}
+        {comment.user_id == localStorage.getItem("rare_user_id") ?
+          <>
+          <i class="fas fa-edit"></i>
+          <i className="fas fa-trash-alt mr-3" onClick={this.deletePostEvent}></i>
+          </>
+        : <div></div>}
+        </h6>
         <p>{comment.content}</p>
-        {comment.user_id == localStorage.getItem("rare_user_id") ? <div><btn>Edit</btn><btn>Delete</btn></div> : <div></div>}
       </div>
     )
   }
