50542784429d1f7016dd3bc45bbdff035f06121c
Geno McNew <genemcnew3@gmail.com>
2020-11-09 19:14:25 -0600

    wip

--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -3,21 +3,21 @@ import { withRouter } from 'react-router-dom';
 import moment from 'moment';
 
 
-class Post extends React.Component {
-
+class Comment extends React.Component {
   render() {
     const { comment } = this.props;
     const creation_date = moment(comment.creation_date).format('MMM Do, yyyy');
+    console.warn(comment.user_id)
+    console.warn(localStorage.getItem("rare_user_id"))
     return(
       <div className="comment">
         <h5>{comment.subject}</h5>
         <h6>{`${comment.user.first_name} ${comment.user.last_name} - ${creation_date}`}</h6>
         <p>{comment.content}</p>
-        
+        {comment.user_id == localStorage.getItem("rare_user_id") ? <div><btn>Edit</btn><btn>Delete</btn></div> : <div></div>}
       </div>
     )
   }
 }
 
-export default withRouter(Post);
-//<h5>{comment.user.first_name} {post.user.last_name}</h5>
+export default withRouter(Comment);
