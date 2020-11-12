6971f9563a6051ada771759cfac64cfd0ec10ab9
NateOwens <nathanieltylerowens@gmail.com>
2020-11-12 17:18:13 -0600

    added delete category

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -2,12 +2,25 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 class Category extends React.Component {
+
+  deleteCategory = () => {
+    const { category } = this.props;
+    return fetch(`http://localhost:8088/categories/${category.id}`, {
+      method: "DELETE"
+    }).then(() => {
+      this.props.history.push('/categories');
+    })
+  }
+
   render() {
     const { category } = this.props;
     return (
       <div className="d-flex justify-content-center cat-list">
-        <button className="btn-primary" id={category.id}>{category.name}</button>
-      </div>
+        <div className="btn-group-sm">
+          <button className="btn-primary" id={category.id}>{category.name}</button>
+          <button className="btn-danger" onClick={this.deleteCategory}><i className="fas fa-skull"></i></button>
+        </div>
+      </div>  
     )
   }
 }
