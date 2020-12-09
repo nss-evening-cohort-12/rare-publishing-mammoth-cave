0f0392aab4292c0f1446750d7f1bfd5f1e1e9d87
Geno McNew <genemcnew3@gmail.com>
2020-12-08 19:08:49 -0600

    adjusted login for djano api

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -25,7 +25,7 @@ export const Register = (props) => {
                 "bio": bio.current.value
             }
     
-            return fetch("http://127.0.0.1:8088/register", {
+            return fetch("http://127.0.0.1:8000/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
