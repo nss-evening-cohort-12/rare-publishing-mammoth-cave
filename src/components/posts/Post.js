45cd22f7ca183f166e4a23ba834ca66c06ba916a
Michael Dotson <thedotson@icloud.com>
2020-12-21 20:25:55 -0600

    chanfged date format and posts print tag labels

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -7,14 +7,14 @@ class Post extends React.Component {
   render() {
     const { post } = this.props;
     const postDetails = `/viewpost/${post.id}`
-    const creation_date = moment(post.creation_date).format('MMM Do, YYY');
+    const creation_date = moment(post.publication_date).format('L');
     return(
       <div className="post-list">
         <h5>{post.user_id.user_id.first_name} {post.user_id.user_id.last_name}</h5>
         <Link to={postDetails}><h5>{post.title}</h5></Link>
         <h5>{creation_date}</h5>
         <h5>{post.category_id.label}</h5>
-        <h5>{post.tags}</h5>
+        <h5>{post.tags.map(tag => <h5>{tag.label}</h5>)}</h5>
       </div>
     )
   }
